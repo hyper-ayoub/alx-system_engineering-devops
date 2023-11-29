d73a135dd6d5a1e01cb51efc33b837d3f73e02dd
hyperayoub <bouagnaayoub@gmail.com>
2023-11-29 12:07:23 -0800

    challenge_number

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
 #The sender phone number or name (including country code if present)
-#The receiver phone number or name (including country code if present)
-#The flags that were used
-puts ARGV[0].scan(/\[(?:from:|to:|flags:)(.*?)\]/).join
+#The receiver phone number or name (including country code if present.
+puts ARGV[0].scan(/\[(?:from:|to:|flags:)(.*?)\]/).join(",")
