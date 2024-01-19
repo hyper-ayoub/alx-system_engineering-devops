eb4c9aa7b0ba89bcd4a937f5d8605c32fce61c42
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-01-20 00:40:27 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,17 @@
-#Installs puppet-lint
+#!/usr/bin/pup
+# installation
+package { 'python3-pip':
+  ensure => installed,
+}
 
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure   => '2.0.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
