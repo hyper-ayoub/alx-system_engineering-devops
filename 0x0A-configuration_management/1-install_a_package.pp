6c9dd3e04892915a4f78f1b1bb51ef7c7d24c8b4
hyperayoub <bouagnaayoub@gmail.com>
2024-01-19 13:15:13 -0800

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,7 @@
-# Install flask from pip3
-
-package { 'pip3':
-  ensure => 'installed',
-}
-
-package { 'flask':
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
+
