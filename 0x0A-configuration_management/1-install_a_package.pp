bea1b8847f6256bdd9ca9af5d8f3f9c984ca3cc5
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-01-19 21:46:30 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 #!/usr/bin/pup
-# install a package Flask (2.0.1)
-package { 'flask':
-  ensure => '2.1.0',
-  provider => 'pip3',
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
+
