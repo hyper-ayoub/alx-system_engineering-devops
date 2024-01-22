b8fbe1ec778358545cb810b79dd98c3efa8117da
hyperayoub <bouagnaayoub@gmail.com>
2024-01-21 16:27:28 -0800

    ssh_project

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,9 @@
+#!/usr/bin/env puppet
+
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+  content => "# SSH client configuration
+              Host *
+              IdentityFile ~/.ssh/school
+              PasswordAuthentication no\n",
+}
