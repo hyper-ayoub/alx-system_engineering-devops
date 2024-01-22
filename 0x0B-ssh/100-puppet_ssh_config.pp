614df48ec7b71470155538693af9db9a85c3392e
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-01-22 11:26:05 +0100

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,15 @@
 #!/usr/bin/env 
 
-file { '/etc/ssh/ssh_config':
-  ensure  => present,
-  content => "# SSH client configuration
-              Host *
-              IdentityFile ~/.ssh/school
-              PasswordAuthentication no",
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
