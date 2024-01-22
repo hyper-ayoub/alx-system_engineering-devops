beed2b85c758abc6e5b3e60d6fc2d6e8ed4ceda1
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-01-22 11:21:38 +0100

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,1 @@
-#!/usr/bin/env puppet
 
-file { '/etc/ssh/ssh_config':
-  ensure  => present,
-  content => "# SSH client configuration
-              Host *
-              IdentityFile ~/.ssh/school
-              PasswordAuthentication no\n",
-}
