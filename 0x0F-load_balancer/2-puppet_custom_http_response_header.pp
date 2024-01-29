2de492c4699db8cf4901ac3ff5760fa32d36b022
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-01-30 00:07:08 +0100

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,29 +1,16 @@
-exec { 'update system':
+# Add a custom HTTP header with Puppet
+exec {'update':
   command => '/usr/bin/apt-get update',
 }
-
-package { 'nginx':
-  ensure  => 'installed',
-  require => Exec['update system'],
+-> package { 'nginx':
+  ensure => installed,
 }
-
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
+-> file_line { 'header_served_by':
+  path  => '/etc/nginx/sites-available/default',
+  match => '^server {',
+  line  => "server {\n\tadd_header X-Served-By \"${hostname}\";",
+  multiple => false,
 }
-
-exec { 'redirect_me':
-  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-  provider => 'shell',
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
 }
-
-file { '/etc/nginx/conf.d/custom_headers.conf':
-  ensure  => present,
-  content => "add_header X-Served-By $::hostname always;\n",
-  notify  => Service['nginx'],
-}
-
-service { 'nginx':
-  ensure  => running,
-  require => [Package['nginx'], File['/etc/nginx/conf.d/custom_headers.conf']],
-}
-
