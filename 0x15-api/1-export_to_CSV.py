879c3143d9a3fa601be0e0d11c143f9f733f0aa7
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-02-19 22:53:30 +0100

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,10 +1,6 @@
-#!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
-
 import csv
 import requests
 import sys
-
 
 if __name__ == "__main__":
     # Get the user ID
@@ -25,8 +21,6 @@ if __name__ == "__main__":
     # Use list comprehension to iterate over the to-do list items
     # Write each item's details (user ID, username, completion status,
     # and title) as a row in the CSV file
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-        ) for t in todos]
+    for t in todos:
+        print(f'"{user_id}","{username}","{t.get("completed")}","{t.get("title")}"')
+
