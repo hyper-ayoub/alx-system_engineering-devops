bbaf88df613c1f6a69ffe8efe53e6aa62f4cd636
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-02-19 22:38:45 +0100

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,36 +1,34 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
+# Python script for Api
+
+"""  export data in the CSV format """
 
 import csv
 import requests
 import sys
 
-
 if __name__ == "__main__":
-    # Get the user ID from the command-line arguments provided to the script
-    user_id = sys.argv[1]
-
-    # Define the base URL for the JSON API
+    # URL
     url = "https://jsonplaceholder.typicode.com/"
 
-    # Fetch user information from the API and
-    #   convert the response to a JSON object
-    user = requests.get(url + "users/{}".format(user_id)).json()
+    # Employee information
+    employee_id = sys.argv[1]
+    user = requests.get(url + "users/{}".format(employee_id)).json()
 
-    # Extract the username from the user data
+    # username
     username = user.get("username")
+    
+    # Fetch TODO list for the employee
+    todos = requests.get(url + "todos", params={"UserId": employee_id}).json()
 
-    # Fetch the to-do list items associated with the
-    #   given user ID and convert the response to a JSON object
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
-
-    # Use list comprehension to iterate over the to-do list items
-    # Write each item's details (user ID, username, completion status,
-    #   and title) as a row in the CSV file
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+    # Filter completed tasks
+    completed = [t.get("title") for t in todos if t.get("completed")]
+    
+    # data export
+    with open("{}.csv".format(employee_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
+            [employee_id, username, t.get("completed"), t.get("title")]
          ) for t in todos]
-    # end of the script
+
 
