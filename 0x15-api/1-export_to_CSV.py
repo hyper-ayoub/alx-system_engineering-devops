e8cf732ca40b5ae4542f3e633d287ded1c66943b
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-02-19 22:26:30 +0100

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,33 +1,37 @@
 #!/usr/bin/python3
-# Python script for Api
-
-"""  export data in the CSV format """
+"""Exports to-do list information for a given employee ID to CSV format."""
 
 import csv
 import requests
 import sys
 
+
 if __name__ == "__main__":
-    # URL
+    # Get the user ID from the command-line arguments provided to the script
+    user_id = sys.argv[1]
+
+    # Define the base URL for the JSON API
     url = "https://jsonplaceholder.typicode.com/"
 
-    # Employee information
-    user = requests.get(url + "users/{}".format(employee_id)).json()
+    # Fetch user information from the API and
+    #   convert the response to a JSON object
+    user = requests.get(url + "users/{}".format(user_id)).json()
 
-    # username
+    # Extract the username from the user data
     username = user.get("username")
-    
-    # Fetch TODO list for the employee
-    todos = requests.get(url + "todos", params={"UserId": employee_id}).json()
 
-    # Filter completed tasks
-    completed = [t.get("title") for t in todos if t.get("completed")]
-    
-    # data export
+    # Fetch the to-do list items associated with the
+    #   given user ID and convert the response to a JSON object
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    # Use list comprehension to iterate over the to-do list items
+    # Write each item's details (user ID, username, completion status,
+    #   and title) as a row in the CSV file
     with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         [writer.writerow(
             [user_id, username, t.get("completed"), t.get("title")]
          ) for t in todos]
+
 
 
