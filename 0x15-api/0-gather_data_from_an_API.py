44032b5c6eb1297b1c9b90c09361c038d6ff3234
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-02-19 22:25:59 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # This is a Python Script
 
-""" Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress """
+""" Python script that, using this REST API """
 
-import sys
 import requests
+import sys
 
 
 if __name__ == "__main__":
