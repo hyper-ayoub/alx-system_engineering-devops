1364aa6a0b6f35e7380d285a949d825b2689be2c
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-03-06 00:06:56 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,30 +1,18 @@
 #!/usr/bin/python3
-# Python code
-
+"""
+this doc for module
+"""
 import requests
 
+headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+
+
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit.
-
-    Returns:
-        int: Total number of subscribers for the subreddit. Returns 0 if the subreddit is invalid.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "MyCustomUserAgent"}  # Set a custom User-Agent
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
+    """method doc"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, allow_redirects=False, headers=headers)
     if response.status_code == 200:
         data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+        return data["data"]["subscribers"]
     else:
         return 0
-
-# Example usage:
-subreddit_name = "learnprogramming"
-subscribers_count = number_of_subscribers(subreddit_name)
-print(f"Subscribers in r/{subreddit_name}: {subscribers_count}")
