502d5dab4b6c1fffda0654aba1a36f19edd3c61d
hyperayoub <bouagnaayoub@gmail.com>
2024-03-05 15:16:49 -0800

    a

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" 3. Count it! """
+import requests
+
+
+def count_words(subreddit, word_list):
+    """
+    parses the title of all hot articles, and prints a sorted count
+    of given keywords (case-insensitive, delimited by spaces.
+    """
+    pass
---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" 2. Recurse it! """
+import requests
+
+
+def recurse(subreddit, hot_list=[]):
+    """
+    returns a list containing the titles of
+    all hot articles for a given subreddit.
+    """
+    pass
