dcc6b795a850430cef01cc5ad445e9a3102000a3
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-04-03 00:31:59 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
+"""
+task 1
+"""
 import requests
 
 headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 
-def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, headers=headers)
 
+def number_of_subscribers(subreddit):
+    """method doc"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, allow_redirects=False, headers=headers)
     if response.status_code == 200:
-        return "OK"  # Subreddit exists
-    elif response.status_code == 404:
-        return "OK"  # Subreddit does not exist
+        data = response.json()
+        return data["data"]["subscribers"]
     else:
-        return 0  # Other errors
+        return 0
