6b690a48e3e79279c76498e752c55c844076137d
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-04-03 00:39:54 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 """
-Get number of subscribers for a given subreddit
+task 1
 """
 import requests
 
+headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+
 
 def number_of_subscribers(subreddit):
-    """Get number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    """method doc"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
         return 0
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {
-        'User-Agent': 'Ubuntu:testing (by /u/ku18m)'
-    }
-    response = requests.get(
-        url, headers=headers, allow_redirects=False, timeout=10)
-    if response.status_code != 200:
-        return 0
-    return response.json().get('data').get('subscribers')
